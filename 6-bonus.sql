d4d0f9e37b5ebb429d0111a35a8171b0f2249127
Brehyma Traore <brehymatraore50@gmail.com>
2024-10-20 01:15:25 +0000

    SQL script that creates a stored procedure AddBonus that adds a new correction for a student.

---
+++ 6-bonus.sql
@@ -0,0 +1,9 @@
+--  SQL script that creates a stored procedure AddBonus that adds a new correction for a student.
+
+DELIMITER $$
+CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+BEGIN
+    INSERT INTO corrections (user_id, project_name, score)
+    VALUES (user_id, project_name, score);
+END$$
+DELIMITER ;
