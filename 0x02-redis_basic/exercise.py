e92a63bf5aa00bc1f5a481592791f1fac9409951
Brehyma Traore <brehymatraore50@gmail.com>
2024-10-23 21:53:08 +0000

    Flushdb called to clear the db

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,6 +12,7 @@ class Cache:
     def __init__(self):
         """Instatiate the class Cache and connect to Redis"""
         self._redis = redis.Redis(host="localhost", port=6379, db=0)
+        self._redis.flushdb()
 
     def store(self, data: Union[int, bytes, str, float]) -> str:
         """Store data in Redis and return the generated UUID key
