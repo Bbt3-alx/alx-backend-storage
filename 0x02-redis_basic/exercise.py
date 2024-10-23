7b2ca2e99c9a27af0583cf4156cea0af9b3f122c
Brehyma Traore <brehymatraore50@gmail.com>
2024-10-23 21:47:09 +0000

    Class to handle caching operations with Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Writing string to Redis"""
+
+
+from typing import Union
+import redis
+import uuid
+
+
+class Cache:
+    """Class to handle caching operations with Redis"""
+    def __init__(self):
+        """Instatiate the class Cache and connect to Redis"""
+        self._redis = redis.Redis(host="localhost", port=6379, db=0)
+
+    def store(self, data: Union[int, bytes, str, float]) -> str:
+        """Store data in Redis and return the generated UUID key
+        Args:
+            data (Union[int, bytes, str, float]): The data to store in Redis.
+        Returns:
+            str: The UUID key under which the data is stored.
+        """
+        random_key = str(uuid.uuid4())
+        self._redis.set(random_key, data)
+        return random_key
