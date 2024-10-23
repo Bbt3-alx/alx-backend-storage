a5474b49b6c7d8e17c3f05ee6667deca87c960fd
Brehyma Traore <brehymatraore50@gmail.com>
2024-10-23 23:49:34 +0000

    pycodestyle warning

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -24,6 +24,7 @@ def call_history(method: Callable) -> callable:
         return output
     return wrapper
 
+
 def replay(method: Callable):
     """Display the history of calls of a particular function"""
     input_key = f"{method.__qualname__}:inputs"
@@ -35,6 +36,8 @@ def replay(method: Callable):
     print(f"{method.__qualname__} was called {len(inputs)}:times")
     for input_args, output in zip(inputs, outputs):
         print(f"{method.__qualname__}(*{input_args.decode('utf-8')}) -> {output.decode('utf-8')}")
+
+
 def count_calls(method: Callable) -> Callable:
     """Decorator to count calls to a method"""
     @functools.wraps(method)
@@ -69,6 +72,7 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
+
     @call_history
     @count_calls
     def get(self, key: str, fn: Optional[Callable] = None) -> Union[int, str, float, bytes, None]:
