1a9c131490c433330a1e7b99cc8194b7b2922c04
Brehyma Traore <brehymatraore50@gmail.com>
2024-10-23 23:36:00 +0000

    Decorator to store the history of inputs and outputs for a function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,6 +8,22 @@ import uuid
 import functools
 
 
+def call_history(method: Callable) -> callable:
+    """Decorator to store the history of inputs and outputs for a function"""
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper function to store inputs and outputs in Redis"""
+        input_key = f"{method.__qualname__}:inputs"
+        output_key = f"{method.__qualname__}:outputs"
+
+        self._redis.rpush(input_key, str(args))
+        output = method(self, *args, **kwargs)
+
+        self._redis.rpush(output_key, str(output))
+
+        return output
+    return wrapper
+
 def count_calls(method: Callable) -> Callable:
     """Decorator to count calls to a method"""
     @functools.wraps(method)
@@ -30,6 +46,7 @@ class Cache:
         self._redis = redis.Redis(host="localhost", port=6379, db=0)
         self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Union[int, bytes, str, float]) -> str:
         """Store data in Redis and return the generated UUID key
@@ -41,7 +58,8 @@ class Cache:
         random_key = str(uuid.uuid4())
         self._redis.set(random_key, data)
         return random_key
-
+    @call_history
+    @count_calls
     def get(self, key: str, fn: Optional[Callable] = None) -> Union[int, str, float, bytes, None]:
         """Retrieve data from Redis and optionally convert it using fn"""
         data = self._redis.get(key)
