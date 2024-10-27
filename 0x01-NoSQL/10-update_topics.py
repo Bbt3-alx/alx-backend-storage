c848577802bb3b1fe1b4e05d6496c840cc4ebe90
Brehyma Traore <brehymatraore50@gmail.com>
2024-10-27 23:57:19 +0000

    updated

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,10 +1,13 @@
-#!/usr/bin/env python3
-"""Change school topics"""
-
-
 def update_topics(mongo_collection, name, topics):
-    """changes all topics of a school document based on the name"""
-    update_all = mongo_collection.update_one(
-            {"name": name},
-            {"$set": {"topics": topics}}
-            )
+    """
+    Updates all topics of a school document based on the name.
+    
+    Parameters:
+    - mongo_collection: the pymongo collection object
+    - name (string): the school name to update
+    - topics (list of strings): the list of topics approached in the school
+    """
+    mongo_collection.update_many(
+        {"name": name},
+        {"$set": {"topics": topics}}
+    )
